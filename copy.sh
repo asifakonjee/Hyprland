dec64f2db670e970a17d8e803fcc2fb14acfcc1a
Linux <asifakonjee@hotmail.com>
2024-01-17 14:28:23 +0530

    Experimental install Scripts

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo cp -r config/* ~/.config/
 sudo cp -r usr/share/fonts/* /usr/share/fonts/
 sudo cp -r usr/share/themes/* /usr/share/themes/
 sudo cp -r usr/share/sdd/themes/* /usr/share/themes/
 sudo fc-cache -fv
+cp -r config/* ~/.config/
