cf028517d385adf07243a4bbff9cee41614d6019
Dr. Asifur Rahman Akonjee <121393032+asifakonjee@users.noreply.github.com>
2024-01-22 18:32:54 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo cp -r usr/share/fonts/* /usr/share/fonts/
-sudo cp -r usr/share/themes/* /usr/share/themes/
-sudo cp -r usr/share/sddm/themes/* /usr/share/sddm/themes/
+sudo cp -r ~/Hyprland/usr/share/fonts/* /usr/share/fonts/
+sudo cp -r ~/Hyprland/usr/share/themes/* /usr/share/themes/
+sudo cp -r ~/Hyprland/usr/share/sddm/themes/* /usr/share/sddm/themes/
 sudo fc-cache -fv
-cp -r config/* ~/.config/
+cp -r ~/Hyprland/config/* ~/.config/
 sudo mv ~/.config/rofi/simple.rasi /usr/share/rofi/themes/
